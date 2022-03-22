db81cab7e26dd42629e4049986f23d2a501ae820
Bella <819767420j@gmail.com>
2022-03-22 17:43:23 +0800

    done with lab7 extra

--- lab7/bstmap/BSTMap.java
+++ lab7/bstmap/BSTMap.java
@@ -1,5 +1,8 @@
 package bstmap;
 
+import edu.princeton.cs.algs4.BST;
+
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Set;
 
@@ -75,30 +78,90 @@ public class BSTMap<K extends Comparable<K>, V>  implements Map61B<K, V> {
             node.left = insert(node.left, key, val);
         } else if (cmp > 0) {
             node.right = insert(node.right, key, val);
+        } else {
+            node.val = val;
         }
-        node.val = val;
         node.num = 1 + size(node.left) + size(node.right);
         return node;
     }
 
     @Override
     public Set<K> keySet() {
-        throw new UnsupportedOperationException();
+        Set<K> BSTSet = new HashSet<>();
+        keySet(root, BSTSet);
+        return BSTSet;
+    }
+
+    private void keySet(BSTNode node, Set<K> BSTSet) {
+        if (node == null) {
+            return;
+        }
+        keySet(node.left, BSTSet);
+        BSTSet.add(node.key);
+        keySet(node.right, BSTSet);
     }
 
     @Override
     public V remove(K key) {
-        throw new UnsupportedOperationException();
+        V value = get(key);
+        root = remove(key, root);
+        return value;
     }
 
     @Override
     public V remove(K key, V value) {
-        throw new UnsupportedOperationException();
+        V val = get(key);
+        if (containsKey(key) && val.equals(value)) {
+            root = remove(key, root);
+            return val;
+        }
+        return value;
+    }
+
+    private BSTNode remove(K key, BSTNode node) {
+        if (node == null) {
+            return null;
+        }
+        int cmp = key.compareTo(node.key);
+        if (cmp < 0) {
+            node.left = remove(key, node.left);
+        } else if (cmp > 0) {
+            node.right = remove(key, node.right);
+        } else {
+            if (node.left == null) {
+                return node.right;
+            }
+            if (node.right == null) {
+                return node.left;
+            }
+            BSTNode tmp = node;
+            node = successor(tmp.right);
+            node.right = deleteSuccessor(tmp.right);
+            node.left = tmp.left;
+        }
+        node.num = 1 + size(node.left) + size(node.right);
+        return node;
+    }
+
+    private BSTNode successor(BSTNode node) {
+        if (node.left == null) {
+            return node;
+        }
+        return successor(node.left);
+    }
+
+    private BSTNode deleteSuccessor(BSTNode node) {
+        if (node.left == null) {
+            return node.right;
+        }
+        node.left = deleteSuccessor(node.left);
+        node.num = size(node.left) + size(node.right) + 1;
+        return node;
     }
 
     @Override
     public Iterator<K> iterator() {
-        throw new UnsupportedOperationException();
+        return keySet().iterator();
     }
 
 }
